f8ddf61c19d45749473bd06e5691ee29027a01e3
Eduardo <edutrix9@hotmail.com>
2021-02-24 03:21:34 -0300

    Viendo instrucciones para formularios en js

--- funciones.js
+++ funciones.js
@@ -1,3 +1,8 @@
-function billetesDe(cantidad, valor) {
-    var valor = document.getElementById("1000").textContent()
+function billetesDe() {
+    var cantidad_1000 = document.arqueo.prueba.value;
+    var resultado = cantidad_1000 * 1000;
+    return resultado;
 }
+
+const elemento = document.getElementById("valor_1000");
+elemento.innerHTML = billetesDe();
